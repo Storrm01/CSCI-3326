ec539a9de63023cb77a7e81bdcf2b53987101968
Matthew <matthewt2356@gmail.com>
2025-02-10 14:49:56 -0600

    Feb 10

--- Loops.java
+++ Loops.java
@@ -16,12 +16,29 @@ public class Loops {
         //     champion = input.next();
         // }
 
-        do{
-            System.out.println("Woohoo! Chiefs");
-            System.out.println("Who is new champion: ");
-            champion = input.next();
-        }while (champion.equals("Chiefs"));
+        // do{
+        //     System.out.println("Woohoo! Chiefs");
+        //     System.out.println("Who is new champion: ");
+        //     champion = input.next();
+        // }while (champion.equals("Chiefs"));
 
-        System.out.println("Cowboys are next!");
+        // System.out.println("Cowboys are next!");
+    
+        // For Loop
+        
+        // for(counter_dec; condition; counter_inc)
+        for(int i = 0; i < 5; i++){
+            System.out.println("value of i is: " + i);
+        }
+
+        int x = 0;
+        for (;;){
+            if(x >=3)
+                break;
+            System.out.println("value of x is: " + x);
+
+            x++;
+        }
     }
+
 }
