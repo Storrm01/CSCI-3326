08554e3447910e3582104ad09e22cdc7c9582d8f
Matthew <matthewt2356@gmail.com>
2025-02-17 12:27:31 -0600

    Practicing Storing Arrays

---
+++ StoringArrays.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class StoringArrays {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter an amount for inventory: ");
+        int size = input.nextInt();
+        int[] arr2 = new int[size];
+
+        for(int i = 0; i < size; i++){
+            System.out.print("Enter each number: ");
+            arr2[i] = input.nextInt();
+        }
+        System.out.println("The numbers you entered were: ");
+        for(int element: arr2){
+            System.out.println(element);
+        }
+    }    
+}
